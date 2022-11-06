b8db9fdefcb9f8a2eeb4e75485528dc49bf8b632
Supriyakaa <pathaksupriya981@gmail.com>
2022-11-06 23:33:34 +0530

    fhy

--- Portfolio/src/Components/Title.jsx
+++ Portfolio/src/Components/Title.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 export function Title() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "MERN Stack Developer",
-          "Problem Solver",
-          "Full Stack Developer"
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+  // return (
+  //   <Typewriter
+  //     options={{
+  //       strings: [
+  //         "MERN Stack Developer",
+  //         "Problem Solver",
+  //         "Full Stack Developer"
+  //       ],
+  //       autoStart: true,
+  //       loop: true,
+  //       deleteSpeed: 50,
+  //     }}
+  //   />
+  // );
 }
