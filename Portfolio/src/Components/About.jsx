367746ca2aedb03962c4b5f29670a6792770c252
Supriyakaa <pathaksupriya981@gmail.com>
2023-02-25 22:45:42 +0530

    daS

--- Portfolio/src/Components/About.jsx
+++ Portfolio/src/Components/About.jsx
@@ -80,8 +80,9 @@ export function About() {
                       MERN Stack Developer having 1200 hours of intensive Coding experience and
 
           able to write clean, reusable, and optimized code. Also,
-          
+
           have a deeper knowledge of Data Structure Algorithms.
+          
           Looking for an opportunity to work in a challenging
           work environment to develop skills and deeper
           understanding of Web development.
