dddbfa51db1dd27f1fe0f0cf8d69bde94936dce6
Supriyakaa <pathaksupriya981@gmail.com>
2023-02-25 22:43:27 +0530

    asdxas

--- Portfolio/src/Components/About.jsx
+++ Portfolio/src/Components/About.jsx
@@ -76,8 +76,9 @@ export function About() {
             data-aos="zoom-in-left"
           >
                       Hello, I am Supriya Pathak from Gujarat... An Energetic and passion-driven
-                      
+
                       MERN Stack Developer having 1200 hours of intensive Coding experience and
+                      
           able to write clean, reusable, and optimized code. Also,
           have a deeper knowledge of Data Structure Algorithms.
           Looking for an opportunity to work in a challenging
